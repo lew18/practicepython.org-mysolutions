fdcdd31fa16ebb99f0de38497a42e5d3e63df43e
lew18 <lew.kurtz@icloud.com>
2018-04-04 21:13:14 -0700

    Put back exercises 1 and 2.

--- ex01-character_input.py
+++ ex01-character_input.py
@@ -1,11 +1,33 @@
+"""
+https://www.practicepython.org
+
+Exercise 1: Character Input
+1 chile
+Create a program that asks the user to enter their name and their age.
+Print out a message addressed to them that tells them the year that they
+will turn 100 years old.
+
+Extras:
+1. Add on to the previous program by asking the user for another number and
+   printing out that many copies of the previous message. (Hint: order of
+   operations exists in Python)
+2. Print out that many copies of the previous message on separate lines.
+   (Hint: the string "\n is the same as pressing the ENTER button)
+
+"""
 
 from datetime import date
 
 today = date.today()
-name = str(input("what is your name? "))
+name = input("what is your name? ")
 age = int(input("how old are you? "))
 if (str(input("Have you had your birthday this year? ")) == "y"):
-    print(name + " will be 100 years old in " + str(today.year + 100 - age))
+    message = (name + " will be 100 years old in " + str(today.year + 100 - age))
 else:
-    print("%s will be 100 years old in %4d." % (name, today.year + 99 - age))
+    # yes, this is strange. The primary purpose is to learn python, so experiment some
+    message = ("%s will be 100 years old in %4d." % (name, today.year + 99 - age))
 
+message_count = int(input("How many times do you want to see the message? "))
+while message_count > 0:
+    print(message)
+    message_count -= 1
--- ex02-odd_or_even.py
+++ ex02-odd_or_even.py
@@ -1,6 +1,21 @@
+"""
+https://www.practicepython.org
 
+Exercise 2: Odd or Even
+1 chile
+Ask the user for a number. Depending on whether the number is even or odd,
+print out an appropriate message to the user.
+Hint: how does an even / odd number react differently when divided by 2?
 
-number = int(input("Give me a number. "))
+Extras:
+1. If the number is a multiple of 4, print out a different message.
+2. Ask the user for two numbers: one number to check (call it num) and
+   one number to divide by (check). If check divides evenly into num,
+   tell that to the user. If not, print a different appropriate message.
+
+"""
+
+number = int(input("Enter an integer: "))
 
 if (number % 4) == 0:
     print(str(number) + " is evenly divisible by 4.")
@@ -8,3 +23,9 @@ elif ((number %2) == 0):
     print(str(number) + " is even.")
 else:
     print(str(number) + " is odd.")
+
+divisor = int(input("Enter a second integer: "))
+if number % divisor:
+    print("%d is not a divisor of %d." % (divisor, number))
+else:
+    print("%d is a divisor of %d." % (divisor, number))
