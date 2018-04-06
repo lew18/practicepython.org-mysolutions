b330835676d337164d06c5d7613008b4df5b6b18
lew18 <lew.kurtz@icloud.com>
2018-04-06 13:55:56 -0700

    revise ex06

--- ex06-string_lists-palindrome.py
+++ ex06-string_lists-palindrome.py
@@ -1,14 +1,23 @@
-def palindrome_checker(s1):
-    is_palindrome = 1
-    i = 0
-    while i <= len(s1)/2 and is_palindrome:
-        if s1[i] != s1[len(s1)-i-1] :
-            is_palindrome = 0
-        i += 1
-    return(is_palindrome)
+"""
+https://www.practicepython.org
 
-s1 = str(input("Enter a string "))
+Exercise 6: String Lists
+2 chilis
+
+Ask the user for a string and print out whether this string is a
+palindrome or not. (A palindrome is a string that reads the same
+forwards and backwards.)
+"""
+
+def palindrome_checker(s1):
+    for i in range(int(len(s1)/2)):
+        if s1[i] != s1[len(s1)-i-1] :
+            return(False)
+    return(True)
+
+
+s1 = input("Enter some text:  ")
 if palindrome_checker(s1.lower()):
-    print(s1 + " is a palindrome")
+    print("'" + s1 + "' is a palindrome")
 else:
-    print(s1 + " is not a palindrome")
+    print("'" + s1 + "' is not a palindrome")
