8eff26187736c081f5c65e062630a64772448b69
lew18 <lew.kurtz@icloud.com>
2018-04-08 21:11:26 -0700

    revised solutions for12 and 13

--- ex12-list_ends.py
+++ ex12-list_ends.py
@@ -1,11 +1,30 @@
-def first_last(start_list):
-    return([start_list[i] for i in [0, len(start_list) - 1] if len(start_list) > 0])
+"""
+https://www.practicepython.org
 
-a = [ 1, 2, 3, 4, 5, "end" ]
+Exercise 12: List Ends
+1 chili
+
+Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25])
+and makes a new list of only the first and last elements of the given list. For
+practice, write this code inside a function.
+
+"""
+def first_last(start_list):
+    return [start_list[i] for i in [0, len(start_list) - 1] if len(start_list) > 0]
+
+def first_last_v2(start_list):
+    if len(start_list) > 0:
+        return [start_list[0], start_list[-1] ]
+    return[]
+
+a = [ 1, 2, 3, 4, 5 ]
 print(first_last(a))
+print(first_last_v2(a))
 
 b = [5]
 print(first_last(b))
+print(first_last_v2(b))
 
 c = []
 print(first_last(c))
+print(first_last_v2(c))
--- ex13-fibonacci.py
+++ ex13-fibonacci.py
@@ -1,16 +1,29 @@
+"""
+https://www.practicepython.org
+
+Exercise 13: Fibonacci
+2 chilis
+
+Write a program that asks the user how many Fibonnaci numbers to generate
+and then generates them. Take this opportunity to think about how you can
+use functions. Make sure to ask the user to enter the number of numbers in
+the sequence to generate.(Hint: The Fibonnaci seqence is a sequence of
+numbers where the next number in the sequence is the sum of the previous
+two numbers in the sequence. The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, …)
+
+"""
+
 def fibn():
 
-    count = int(input("How many Fibonacci numbers do you want? "))
+    count = int(input("How many Fibonacci numbers do you want? (Enter a number > 1): "))
+    if count <= 1:
+        fibn_list = "Fiboacci sequence always has at least 2 digits."
+    else:
+        fibn_list = [1, 1]
+        for i in range(2, count):
+            fibn_list.append(fibn_list[-2] + fibn_list[-1])
+    return fibn_list
 
-    b1 = b2 = 1
-    print("Fibonacci sequence to %d entries is %d, %d" % (count, b2, b1), end="")
 
-    for i in range(2, count):
-        new = b1 + b2
-        b2 = b1
-        b1 = new
-        print(", %d" % b1, end="")
-
-    print()
-
-fibn()
+ans = fibn()
+print(ans)
