484bce13a073706cf1e7025382bd6a22737e1547
lew18 <lew.kurtz@icloud.com>
2018-05-14 13:06:05 -0700

    first two in the 30s

--- ex30-pick_word.py
+++ ex30-pick_word.py
@@ -1,5 +1,21 @@
-""" 
-exercise 30, pick a random word from sowpos.webarchive
+"""
+https://www.practicepython.org
+
+Exercise 30: Pick word
+2 chilis
+
+This exercise is Part 1 of 3 of the Hangman exercise series. The
+other exercises are: Part 2 and Part 3.
+
+In this exercise, the task is to write a function that picks a
+random word from a list of words from the SOWPODS dictionary.
+Download this file and save it in the same directory as your
+Python code. This file is Peter Norvig’s compilation of the
+dictionary of words used in professional Scrabble tournaments.
+Each line in the file contains a single word.
+
+Hint: use the Python random library for picking a random word.
+
 One of Michele's samples has
 words = list(word_file)
 which is pretty sweet, but it doesn't strip out the /n
@@ -11,7 +27,7 @@ import random
 
 if __name__ == '__main__':
     word_list = []
-    with open('sowpods.webarchive', 'r') as word_file:
+    with open('sowpods.txt', 'r') as word_file:
         word = word_file.readline().strip()
         while word:
             word_list.append(word)
--- ex31-guess_letters-hangman.py
+++ ex31-guess_letters-hangman.py
@@ -1,5 +1,39 @@
 """ 
-exercise 31, guess a random word from sowpos.webarchive
+https://www.practicepython.org
+
+Exercise 31: Guess Letters
+2 chilis
+
+This exercise is Part2 of 3 of the Hangman exercise series. The other exercises
+are: Part 1 and Part 3.
+
+Let’s continue building Hangman. In the game of Hangman, a clue word is given
+by the program that the player has to guess, letter by letter. The player guesses
+one letter at a time until the entire word has been guessed. (In the actual game,
+the player can only guess 6 letters incorrectly before losing).
+
+Let’s say the word the player has to guess is “EVAPORATE”. For this exercise,
+write the logic that asks a player to guess a letter and displays letters in
+the clue word that were guessed correctly. For now, let the player guess an
+infinite number of times until they get the entire word. As a bonus, keep track
+of the letters the player guessed and display a different message if the player
+tries to guess that letter again. Remember to stop the game when all the letters
+have been guessed correctly! Don’t worry about choosing a word randomly or
+keeping track of the number of guesses the player has remaining - we will deal
+with those in a future exercise.
+
+An example interaction can look like this:
+
+>>> Welcome to Hangman!
+_ _ _ _ _ _ _ _ _
+>>> Guess your letter: S
+Incorrect!
+>>> Guess your letter: E
+E _ _ _ _ _ _ _ E
+...
+And so on, until the player gets the word.
+
+
 first line of Michele's file is #! /usr/bin/python3
 """
 
@@ -16,14 +50,13 @@ def get_word_list(file_to_read):
 
 def play_hangman(word):
     char_list = list(word)
-    answer = [' ']* len(word)
-    misses = [ ]
-    mistakes = 0
-    while ' ' in answer and mistakes < 7:
-        print("\n answer is " + str(answer))
-        print("mistakes so far " + str(misses))
+    answer = ['_'] * len(word)
+    mistakes = [ ]
+    while '_' in answer and len(mistakes) < 7:
+        print("\n answer is " + ''.join(answer) + " (" + str(len(word)) + " letters)")
+        print("mistakes so far " + ''.join(mistakes))
         letter = input('Guess a letter ').upper()
-        if letter in answer or letter in misses:
+        if letter in answer or letter in mistakes:
             print("you already guessed " + letter)
         else:
             found = False
@@ -32,17 +65,16 @@ def play_hangman(word):
                     answer[i] = letter
                     found = True
             if not found:
-                misses.append(letter)
-                mistakes += 1
+                mistakes.append(letter)
             
-    if mistakes < 7:
-        print('You guessed ' + word + ' with only ' + str(mistakes) + ' wrong guesses')
+    if len(mistakes) < 7:
+        print('You guessed ' + word + ' with ' + str(len(mistakes)) + ' wrong guesses')
     else:
         print('The word is ' + word)
 
 if __name__ == '__main__':
     
-    word_list = get_word_list('sowpods.webarchive')
+    word_list = get_word_list('sowpods.txt')
     word = word_list[random.randint(0, len(word_list) - 1)]
 
     play_hangman(word)
